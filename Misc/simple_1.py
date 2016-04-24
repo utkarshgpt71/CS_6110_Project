4b8eb8ec6a213b4c5e770e2623266fbd1b4c5c73
utk7 <utkarshgpt71@gmail.com>
2016-04-23 21:58:08 -0600

    Tried the Boolector Python

---
+++ Misc/simple_1.py
@@ -0,0 +1,27 @@
+from boolector import Boolector
+
+btor = Boolector();
+btor.Set_opt("model_gen", 1)
+btor.Set_opt("incremental", 1)
+print "Hello"
+
+x = btor.Var(4, "x")
+y = btor.Var(4, "y")
+
+btor.Assert(x * y > 13)
+
+result = btor.Sat()
+
+#print result
+
+while (result == 10):
+	print 'SAT'
+	print int(x.assignment,2), 
+	print int(y.assignment,2)
+	x_val = int(x.assignment,2)
+	y_val = int(y.assignment,2)
+	btor.Assert(x != x_val)
+	btor.Assert(y != y_val)
+	result = btor.Sat()
+
+print "UNSAT"
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CS 6110 Project
 
 Author: Utkarsh Gupta
+
 Affiliation: University of Utah 
 
 Application: Verification of Arithmetic Datapaths.
